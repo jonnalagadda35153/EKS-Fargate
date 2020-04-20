0d1f1bce01bf5e1094fcadc52f1c223c40f3ab91
Jonnlagadda <jjonna@a483e79a66f9.ant.amazon.com>
2020-04-19 22:57:04 -0400

    Adding Monitoring Filues

--- EKS_Fargate_Logging/docker-spring-boot/src/main/java/com/mkyong/StartApplication.java
+++ EKS_Fargate_Logging/docker-spring-boot/src/main/java/com/mkyong/StartApplication.java
@@ -1,13 +1,20 @@
 package com.mkyong;
 
-
-import org.apache.logging.log4j.*;
+import com.sumologic.http.aggregation.SumoBufferFlusher;
+import com.sumologic.http.sender.ProxySettings;
+import com.sumologic.http.sender.SumoHttpSender;
+import com.sumologic.http.queue.BufferWithEviction;
+import com.sumologic.http.queue.BufferWithFifoEviction;
+import java.io.IOException;
+import static com.sumologic.http.queue.CostBoundedConcurrentQueue.CostAssigner;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @SpringBootApplication
 @Controller
@@ -24,7 +31,6 @@ public class StartApplication {
 
 
     public static void main(String[] args) {
-        LOG.debug("I am here");
         LOG.info("This is a sample log message");
         SpringApplication.run(StartApplication.class, args);
     }
